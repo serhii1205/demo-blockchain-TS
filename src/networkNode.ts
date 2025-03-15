d5055961576d03a8221f6cfb9a32ca6917470660
serhii1205 <freemstage@gmail.com>
2025-03-15 13:28:01 +0200

    feat: adding broadcast endpoints

--- src/networkNode.ts
+++ src/networkNode.ts
@@ -43,4 +43,42 @@ app.get('/mine', (req:any, res: any) => {
 	res.json({msg: 'New block mined successfully', block: newBlock});
 });
 
+// register a node and broadcast it the network
+// this endpoint wil send in url of a new node that we want to add to the network
+app.post('/register-and-broadcast-node', (req: any, res: any) => {
+	const {nodeUrl} = req.body;
+	
+	if (tntCoin.networkNodes.includes(nodeUrl)) {
+		return;
+	}
+	tntCoin.networkNodes.push(nodeUrl);
+
+	const registerNodePromises = tntCoin.networkNodes.map(networkNodeUrl => {
+		return fetch(`${networkNodeUrl}/register-node`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ nodeUrl })
+		});
+	});
+
+	 Promise.all(registerNodePromises).then(() => {
+		return fetch(`${nodeUrl}/register-nodes-bulk`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ networkNodes: [...tntCoin.networkNodes, tntCoin.currentNodeUrl] })
+		});
+	 }).then(data => {
+		res.json({ msg: `New node ${data} registered with network successfully`});
+	 });
+});
+
+// register a node with the network
+app.post('/register-node', (req: any, res: any) => {
+
+});
+
+// register multiple odes at once
+app.post('/register-nodes-bulk', (req: any, res: any) => {});
+	
+
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
